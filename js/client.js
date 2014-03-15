a0e26e61b1c0c1ed0e631af8d29b98071e67dae8
Steve Leonard <xsleonard@gmail.com>
2014-03-15 00:19:43 -0400

    rpc interface + little demo

--- js/client.js
+++ js/client.js
@@ -6,7 +6,7 @@ function NewRPCClient(config) {
     if (config === undefined) {
         config = {};
     }
-    var async = (config.async === undefined) ? false : true;
+    var async = (config.async === undefined) ? false : config.async;
     return {
         // A default prefix for rpc method names
         service_name: config.service_name || '',
@@ -14,16 +14,11 @@ function NewRPCClient(config) {
         async: async,
         // Player data cached here after signup or login
         player: null,
-        rpc: new $.JsonRpcClient({
-            ajaxUrl: '/api',
-            async: async,
-        }),
-
         history: [],
 
         // Returns the last element in the history
         last: function() {
-            return history[history.length - 1];
+            return this.history[this.history.length - 1];
         },
 
         signup: function(username, password, email) {
@@ -43,7 +38,7 @@ function NewRPCClient(config) {
                         method: 'signup',
                         args: null,
                         result: data,
-                        error: null,
+                        error: data.errors,
                     });
                 },
                 error: function(error) {
@@ -73,7 +68,7 @@ function NewRPCClient(config) {
                         method: 'login',
                         args: null,
                         result: data,
-                        error: null,
+                        error: data.errors,
                     });
                 },
                 error: function(error) {
@@ -100,7 +95,7 @@ function NewRPCClient(config) {
                         method: 'logout',
                         args: null,
                         result: data,
-                        error: null,
+                        error: data.errors,
                     });
                 },
                 error: function(error) {
@@ -114,21 +109,30 @@ function NewRPCClient(config) {
             });
         },
 
+        _rpc: new $.JsonRpcClient({
+            ajaxUrl: '/api',
+            async: async,
+        }),
+
         // Makes rpc request to method (e.g. 'info.battle').  Add arguments
         // as necessary for that rpc method.
         rpc: function(method) {
-            var fullmethod = this.service_name + '.' + method;
+            var fullmethod = method;
+            if (this.service_name != '') {
+                fullmethod = this.service_name + '.' + method;
+            }
             var args = Array.prototype.slice.call(arguments, 1);
-            this.rpc.call(fullmethod, args,
+            var that = this;
+            this._rpc.call(fullmethod, args,
                 function(result) {
-                    this._add_to_history({
+                    that._add_to_history({
                         method: method,
                         args: args,
                         result: result,
                         error: null,
                     });
                 }, function(error) {
-                    this._add_to_history({
+                    that._add_to_history({
                         method: method,
                         args: args,
                         result: null,
@@ -149,11 +153,15 @@ function run_demo() {
     }
     var client = NewRPCClient(config);
 
-    client.signup('testuser', 'testpass', 'test@example.com');
-    var signup = client.last();
-    console.log(signup);
-    if (signup.error !== null) {
-        return
+    client.login('testuser', 'testpass');
+    var login = client.last();
+    if (login.error) {
+        client.signup('testuser', 'testpass', 'test@example.com');
+        var signup = client.last();
+        console.log(signup);
+        if (signup.error) {
+            return
+        }
     }
     client.rpc('vestibule.create');
     var vest = client.last();
@@ -161,13 +169,15 @@ function run_demo() {
     if (vest.error !== null) {
         return
     }
-    var world = client.rpc('vestibule.start', vest.result.uid);
+    console.log("Starting vestibule (creates world, is slow)");
+    client.rpc('vestibule.start', vest.result.vestibule.uid);
+    var world = client.last();
     console.log(world);
-    if (world.error !== null) {
-        return
-    }
+    return world;
 }
 
 $(document).ready(function() {
-    run_demo();
+    var world = run_demo();
+    console.log("World created:");
+    console.log(world);
 });
